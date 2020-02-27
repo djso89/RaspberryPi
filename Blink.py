2260c0f24a7730ebf127f1818730de7194e7f910
djso89 <danielso89@gmail.com>
2020-02-26 21:01:40 -0800

    added some comments

--- Blink.py
+++ Blink.py
@@ -1,8 +1,14 @@
+#Description: This is a program that turns LED on and off for every
+#300 ms.
+#importing the module
 import RPi.GPIO as GPIO
 import time
 
+#disable the warning
 GPIO.setwarnings(False)
+#Pin numbering
 GPIO.setmode(GPIO.BCM)
+#set up the channel. configure pin 18 as output
 GPIO.setup(18, GPIO.OUT)
 try:
     while True:
